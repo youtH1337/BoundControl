ab6ee312b43db5b421d176e8c34bbd57c3f6d9d7
youtH1337 <44013365+youtH1337@users.noreply.github.com>
2020-04-18 16:25:57 +0500

    Добавьте файлы проекта.

---
+++ BoundControl/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BoundControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.ds = new System.Data.DataSet();
+            this.uSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(425, 77);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Университет";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(390, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(32, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(425, 88);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Факультет";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(16, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(390, 24);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox3);
+            this.groupBox3.Location = new System.Drawing.Point(32, 190);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(425, 83);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Студент";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(16, 45);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(390, 24);
+            this.comboBox3.TabIndex = 0;
+            // 
+            // ds
+            // 
+            this.ds.DataSetName = "NewDataSet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 297);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Университеты";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Data.DataSet ds;
+        private System.Windows.Forms.BindingSource uSource;
+        private System.Windows.Forms.BindingSource fSource;
+        private System.Windows.Forms.BindingSource sSource;
+    }
+}
+
---
+++ BoundControl/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace BoundControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Connection();
+            BindingSourceProperties();
+            ComboBoxProperties();
+        }
+        void Connection()
+        {
+            string sConnectionStr = "Server=localhost; Port=5432; User Id=postgres; Password=1234765; Database=lab4uchbd";
+            NpgsqlConnection sConnection = new NpgsqlConnection(sConnectionStr);
+            sConnection.Open();
+            DataSetFill(sConnection);
+            sConnection.Close();
+        }
+
+        void DataSetFill(NpgsqlConnection sConnection)
+        {
+            string uSelect = "SELECT * FROM universities";
+            string fSelect = "SELECT * FROM faculties";
+            string sSelect = "SELECT * FROM students";
+            NpgsqlDataAdapter uAdapter = new NpgsqlDataAdapter(uSelect, sConnection);
+            NpgsqlDataAdapter fAdapter = new NpgsqlDataAdapter(fSelect, sConnection);
+            NpgsqlDataAdapter sAdapter = new NpgsqlDataAdapter(sSelect, sConnection);
+            uAdapter.Fill(ds, "universities");
+            fAdapter.Fill(ds, "faculties");
+            ds.Relations.Add("uni_off_fk", ds.Tables["universities"].Columns["id"], ds.Tables["faculties"].Columns["u_id"]);
+            sAdapter.Fill(ds, "students");
+            ds.Relations.Add("fac_emp_fk", ds.Tables["faculties"].Columns["id"], ds.Tables["students"].Columns["f_id"]);
+        }
+
+        void BindingSourceProperties()
+        {
+            uSource.DataSource = ds;
+            uSource.DataMember = "universities";
+            fSource.DataSource = uSource;
+            fSource.DataMember = "uni_off_fk";
+            sSource.DataSource = fSource;
+            sSource.DataMember = "fac_emp_fk";
+        }
+
+        void ComboBoxProperties()
+        {
+            comboBox1.DataSource = uSource;
+            comboBox1.DisplayMember = "name_uni";
+            comboBox2.DataSource = fSource;
+            comboBox2.DisplayMember = "name_fac";
+            comboBox3.DataSource = sSource;
+            comboBox3.DisplayMember = "name_st";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
